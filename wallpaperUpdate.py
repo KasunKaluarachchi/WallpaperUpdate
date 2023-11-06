dfb75ded2baba32f3e7a05fb518fdd47009a22d2
Kasun Kaluarachchi <kasunkaluarchchi10@gmail.com>
2023-11-06 06:21:27 +0100

    Update wallpaperUpdate.py

--- wallpaperUpdate.py
+++ wallpaperUpdate.py
@@ -3,7 +3,6 @@ import ctypes
 import os
 
 # Retrieve image from API
-image_url1 = "https://pixabay.com/api/?key=40439767-62fcf0a43d2a3a3b9856f80eb&q=yellow+flowers&image_type=photo&pretty=true"
 image_url = "https://picsum.photos/1024/768"
 response = requests.get(image_url)
 image_path = "temp_image.jpg"
